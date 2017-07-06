2020df90e8757b6d1bafa129d9d9409f035b3441
Nick Mackenzie <nik@pml.ac.uk>
2017-07-06 16:00:24 +0100

    Added example vscode settings, since this is the contribution guide.

--- style_example/test/server/app.js
+++ style_example/test/server/app.js
@@ -6,12 +6,12 @@ import app from '../../lib/server/app';
 chai.use(chaiHttp);
 let expect = chai.expect;
 
-describe('app', function () {
-  describe('GET /swagger.json', function () {
-    it('should return the swagger json', function (done) {
+describe('app', function() {
+  describe('GET /swagger.json', function() {
+    it('should return the swagger json', function(done) {
       chai.request(app)
         .get('/swagger.json')
-        .end(function (err, res) {
+        .end(function(err, res) {
           expect(res).to.have.status(200);
           expect(res).to.be.json;
           done();
