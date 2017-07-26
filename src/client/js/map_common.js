b071b48285f20c4043760e08d3b319b7b1a9e523
Nicholas J R Bailey <njrbailey@outlook.com>
2017-07-26 11:14:51 +0100

    Common map tiles and views added for OpenLayers

--- src/client/js/map_common.js
+++ src/client/js/map_common.js
@@ -1,48 +1,47 @@
-export let baselayers = [
-  {
-    id: 'EOX',
-    title: 'EOX',
-    description: 'EPSG:4326 only',
-    projections: ['EPSG:4326'],
-    source: {
-      url: 'https://tiles.maps.eox.at/wms/?',
-      crossOrigin: null,
-      params: {
-        type: 'wms',
-        LAYERS: 'terrain-light',
-        VERSION: '1.1.1',
-        wrapDateLine: true,
+export let baselayers = [{
+      id: 'EOX',
+      title: 'EOX',
+      description: 'EPSG:4326 only',
+      projections: ['EPSG:4326'],
+      source: {
+         type: 'wms',
+         url: 'https://tiles.maps.eox.at/wms/?',
+         crossOrigin: null,
+         params: {
+            LAYERS: 'terrain-light',
+            VERSION: '1.1.1',
+            SRS: 'EPSG:4326',
+            FORMAT: 'image/jpeg',
+            wrapDateLine: true,
+         },
+         attributions: ['EOX'],
       },
-      attributions: ['EOX'],
-    },
-  },
-  {
-    tile: {
+   },
+   {
       id: 'OSM',
       title: 'OSM',
       description: 'EPSG:3857 only',
       projections: ['EPSG:3857'],
       source: {
-        type: 'osm',
+         type: 'osm',
       },
-    },
-    // view: new ol.View({
-    //   center: ol.proj.fromLonLat([37.41, 8.82]),
-    //   zoom: 4,
-    // }),
-  },
-  {
-    tile: {
+
+      // view: new ol.View({
+      //   center: ol.proj.fromLonLat([37.41, 8.82]),
+      //   zoom: 4,
+      // }),
+   },
+   {
       id: 'GEBCO',
       title: 'GEBCO',
       projections: ['EPSG:4326', 'EPSG:3857'],
       source: {
-        type: 'wms',
-        url: 'https://www.gebco.net/data_and_products/gebco_web_services/web_map_service/mapserv?',
-        crossOrigin: null,
-        params: {LAYERS: 'gebco_08_grid', VERSION: '1.1.1', FORMAT: 'image/jpeg', wrapDateLine: true},
-        attributions: ['GEBCO'],
+         type: 'wms',
+         url: 'https://www.gebco.net/data_and_products/gebco_web_services/web_map_service/mapserv?',
+         crossOrigin: null,
+         params: { LAYERS: 'gebco_08_grid', VERSION: '1.1.1', SRS: 'EPSG:4326', FORMAT: 'image/jpeg', wrapDateLine: true },
+         attributions: ['GEBCO'],
       },
-    },
-  },
+   },
+
 ];
