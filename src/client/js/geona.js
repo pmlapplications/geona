58b9634a2ebdfa0879ee3852cf6642f737a2609d
Nicholas J R Bailey <njrbailey@outlook.com>
2017-08-09 10:09:46 +0100

    Support + config options for Terms + Conditions and Splash Screen

--- src/client/js/geona.js
+++ src/client/js/geona.js
@@ -8,7 +8,6 @@ import * as ol from './map_openlayers';
 window.templates = templates;
 window.$ = $;
 
-let geona;
 
 /**
  * The entry class for Geona.
@@ -19,29 +18,51 @@ export class Geona {
    * @param  {Object} clientConfig A JSON client config.
    */
   constructor(clientConfig) {
-    geona = this;
-    geona.config = new Config(clientConfig);
-    geona.loadInitialTemplate_();
+    this.config = new Config(clientConfig);
+    this.loadInitialTemplate_();
   }
 
   /**
    * The first screen the user sees if terms and conditions are displayed, in which case the map will not be loaded
    * unless the accept button is clicked.
+   * @private
    */
   loadInitialTemplate_() {
-    let parentDivId = geona.config.get('divId');
-    $(parentDivId).html(templates.geona({parentDivId: parentDivId}));
-    if (geona.config.get('displayTermsAndConditions')) {
-      $(parentDivId + ' .geona-overlay').append(templates.terms_and_conditions({}));
-      $(parentDivId + ' .geona-overlay').css('background-image', 'url(' + geona.config.get('termsAndConditionsImage') + ')');
-      $(parentDivId + ' .agree-terms-and-conditions').click( function() {
+    let parentDivId = this.config.get('divId');
+    if (this.config.get('displayTermsAndConditions')) {
+      $(parentDivId).html(templates.terms_and_conditions({}));
+      $(parentDivId + ' .geona-overlay').css('background-image', 'url(' + this.config.get('termsAndConditionsImage') + ')');
+      $(parentDivId + ' .agree-terms-and-conditions').click( () => {
         $(parentDivId + ' .terms-and-conditions').toggleClass('inactive', true);
         $(parentDivId + ' .geona-overlay').toggleClass('inactive', true);
-        geona.loadMainTemplate_(parentDivId);
-        // Save that T&C accepted
+        this.loadMainTemplate_(parentDivId);
+        // TODO Save that T&C accepted
       });
     } else {
-      geona.loadMainTemplate_(parentDivId);
+      this.loadMainTemplate_(parentDivId);
+    }
+  }
+
+  /**
+   * Finds the correct div to put the map in, then constructs the map.
+   * @private
+   */
+  initialiseMapDiv_() {
+    // Get the HTMLElement div to put the map in
+    let mapDiv = $(this.config.get('divId') + ' .geona-map')[0];
+
+    // TODO this should perhaps go a in seperate init method that returns a callback or promise
+    switch (this.config.get('map.library')) {
+      case 'openlayers':
+        ol.init(() => {
+          this.map = new ol.OlMap(this.config.get('map'), mapDiv);
+        });
+        break;
+      case 'leaflet':
+        leaflet.init(() => {
+          this.map = new leaflet.LMap(this.config.get('map'), mapDiv);
+        });
+        break;
     }
   }
 
@@ -50,70 +71,20 @@ export class Geona {
    * @private
    */
   loadMainTemplate_(parentDivId) {
-    // Get the HTMLElement div to put the map in
-    let mapDiv = $(geona.config.get('divId') + ' .geona-map')[0];
+    $(parentDivId).html(templates.geona({parentDivId: parentDivId}));
+    this.initialiseMapDiv_();
 
-    // TODO this should perhaps go a in seperate init method that returns a callback or promise
-    switch (geona.config.get('map.library')) {
-      case 'openlayers':
-        ol.init(() => {
-          geona.map = new ol.OlMap(geona.config.get('map'), mapDiv);
-        });
-        break;
-      case 'leaflet':
-        leaflet.init(() => {
-          geona.map = new leaflet.LMap(geona.config.get('map'), mapDiv);
-        });
-        break;
-    }
-
-    if (geona.config.get('displaySplashScreen')) {
-      $(parentDivId + ' .geona-overlay').css('background-image', 'url(' + geona.config.get('splashScreenImage') + ')');
+    if (this.config.get('displaySplashScreen')) {
+      $(parentDivId + ' .geona-overlay').css('background-image', 'url(' + this.config.get('splashScreenImage') + ')');
       $(parentDivId + ' .geona-overlay').toggleClass('inactive', false);
-      $(parentDivId + ' .geona-overlay').append(templates.splash_screen({splashMessage: geona.config.get('splashScreenHtml')}));
-      $(parentDivId + ' .load-previous-map').click( function() {
-        // If previous, load, otherwise just normal map
+      $(parentDivId + ' .geona-overlay').append(templates.splash_screen({splashMessage: this.config.get('splashScreenHtml')}));
+      $(parentDivId + ' .load-previous-map').click( () => {
+        // TODO If previous, load, otherwise just normal map
         alert('Load from state');
       });
-      $(parentDivId + ' .start-building-map').click( function() {
+      $(parentDivId + ' .start-building-map').click( () => {
         $(parentDivId + ' .geona-overlay').toggleClass('inactive', true);
       });
     }
-
-    // if (geona.config.get('displaySplashScreen')) {
-    //   $(parentDivId + ' .geona-overlay').append(templates.splash_screen({splashMessage: geona.config.get('splashScreenHtml')}));
-
-    //   $(parentDivId + ' .load-previous-map').click( function() {
-    //     // If previous, load, otherwise just normal map
-    //     alert('Load from state');
-    //   });
-    //   let displayTermsAndConditions = geona.config.get('displayTermsAndConditions');
-    //   $(parentDivId + ' .start-building-map').click( function() {
-    //     if (displayTermsAndConditions) { // && T&C not accepted {
-    //       $(parentDivId + ' .splash-screen').addClass('inactive');
-    //       $(parentDivId + ' .geona-overlay').append(templates.terms_and_conditions({}));
-    //       $(parentDivId + ' .agree-terms-and-conditions').click( function() {
-    //         $(parentDivId + ' .geona-overlay').addClass('inactive');
-    //         // Save that T&C accepted
-    //       });
-    //       $(parentDivId + ' .decline-terms-and-conditions').click( function() {
-    //         $(parentDivId + ' .terms-and-conditions').addClass('inactive');
-    //         $(parentDivId + ' .splash-screen').removeClass('inactive');
-    //         // Save that T&C accepted
-    //       });
-    //     } else {
-    //       $(parentDivId + ' .geona-overlay').addClass('inactive');
-    //     }
-    //   });
-    //   // } else {
-    //   // $(parentDivId + ' .geona-overlay').addClass('inactive');
-    //   // }
-    // } else if (geona.config.get('displayTermsAndConditions')) {
-    //   $(parentDivId + ' .geona-overlay').append(templates.terms_and_conditions({}));
-    //   $(parentDivId + ' .agree-terms-and-conditions').click( function() {
-    //     $(parentDivId + ' .geona-overlay').addClass('inactive');
-    //   // Save that T&C accepted
-    //   });
-    // }
   }
 }
