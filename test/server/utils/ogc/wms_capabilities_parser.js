d83b6e1c01264bb0e0aab75f6e4d6079987be5bc
Nick Mackenzie <nik@pml.ac.uk>
2017-09-13 10:30:41 +0100

    Fixed tests. Added test for WMS 1.1.1 capabilities.

--- test/server/utils/ogc/wms_capabilities_parser.js
+++ test/server/utils/ogc/wms_capabilities_parser.js
@@ -9,11 +9,20 @@ chai.use(chaiHttp);
 let expect = chai.expect;
 
 describe('server/utils/ogc/wms_capabilities_parser', () => {
-  let resPath = path.join(global.test.resPath, 'server/utils/ogc');
-  let expPath = path.join(global.test.expPath, 'server/utils/ogc');
+  let resPath;
+  let expPath;
+
+  before(() => {
+    resPath = path.join(global.test.resPath, 'server/utils/ogc');
+    expPath = path.join(global.test.expPath, 'server/utils/ogc');
+  });
 
   it('should correctly parse a WMS 1.3.0 capabilities', () => {
     wmsParserTest(resPath + '/wms_1_3_0_capabilities_cci.xml', expPath + '/wms_1_3_0_capabilities_cci.json');
+  });
+
+  it('should correctly parse a WMS 1.1.1 capabilities', () => {
+    wmsParserTest(resPath + '/wms_1_1_1_capabilities_cci.xml', expPath + '/wms_1_1_1_capabilities_cci.json');
   });
 
   function wmsParserTest(xmlPath, expectedPath) {
