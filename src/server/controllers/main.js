f343dbee6704e1460291396834dfba072f327af2
Ben Calton <bac@pml.ac.uk>
2017-09-14 16:24:57 +0100

    Add server config when rendering index

--- src/server/controllers/main.js
+++ src/server/controllers/main.js
@@ -1,10 +1,12 @@
 /** @module controllers/main */
 
+import * as config from '../config';
+
 /**
  * Render the index page
  * @param  {Object} req Express request
  * @param  {Object} res Express response
  */
 export function index(req, res) {
-  res.render('index');
+  res.render('index', config.server.getProperties());
 }
