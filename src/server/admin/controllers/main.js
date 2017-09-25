12a33695d297371fd4ba11fcb753450735291ac1
Ben Calton <bac@pml.ac.uk>
2017-09-25 18:43:52 +0100

    Add POST function to handle setup form results

--- src/server/admin/controllers/main.js
+++ src/server/admin/controllers/main.js
@@ -7,7 +7,12 @@
  * @param {any} res 
  */
 import * as config from '../../config';
+import * as requestUtils from '../../utils/request';
 import * as menu from '../templates/menu';
+import convict from 'convict';
+import * as path from 'path';
+
+import * as schema from '../../../common/config_schema.js';
 
 /**
  * Renders the main index page for the administration application
@@ -62,6 +67,11 @@ export function login(req, res) {
  */
 export function setup(req, res) {
   let data = {
+    data: {
+      authorizedOrigin: requestUtils.getAppBasePath(req),
+      authorizedRedirectURI: requestUtils.getAppBasePath(req) + '/user/auth/google/callback',
+      GitHubCallbackUrl: requestUtils.getAppBasePath(req) + '/user/auth/github/callback',
+    },
     config: config.server.getProperties(),
     template: 'setup',
     menu: menu.getMenu('/admin'),
@@ -74,4 +84,63 @@ export function setup(req, res) {
   } else {
     res.render('admin_template', data);
   }
+}
+
+/**
+ * 
+ * 
+ * @export
+ * @param {any} req 
+ * @param {any} res 
+ */
+export function setupPost(req, res) {
+  let data = req.body;
+  let providers = [];
+
+  let serverConfig = convict(schema.server);
+  serverConfig.loadFile(path.join(__dirname, '../../../../config/config_server.json'));
+
+  // Add the Google details if they are complete
+  if (data.google_clientId.length !== 0 && data.google_secret.length !== 0) {
+    let provider = {
+      google: {
+        scope: 'https://www.googleapis.com/auth/userinfo.email',
+        clientid: data.google_clientId,
+        clientsecret: data.google_secret,
+        callback: requestUtils.getAppBasePath(req) + '/user/auth/google/callback',
+        prompt: 'select_account',
+      },
+    };
+    providers.push(provider);
+  }
+
+  // Add the GitHub details if they are complete
+  if (data.github_clientId.length !== 0 && data.github_secret.length !== 0) {
+    let provider = {
+      github: {
+        scope: 'user:email',
+        clientid: data.github_clientId,
+        clientsecret: data.github_secret,
+        callback: requestUtils.getAppBasePath(req) + '/user/auth/github/callback',
+      },
+    };
+    providers.push(provider);
+  }
+
+  serverConfig.load({
+    OAuth: providers,
+  });
+
+  // validate the config before saving it
+  try {
+    serverConfig.validate();
+  } catch (e) {
+    let responseData = {
+      template: 'error',
+      data: e,
+    };
+    res.status(500).render('admin_error', responseData);
+  }
+
+  res.redirect('../admin');
 }
