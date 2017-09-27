c9c4ea56bf3cfcffbdde1d828c412e4c8a30e148
Ben Calton <bac@pml.ac.uk>
2017-09-27 13:41:09 +0100

    Passport strategy with hard coded credentials

--- src/server/routers/user.js
+++ src/server/routers/user.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import passport from 'passport';
 import GoogleStrategy from 'passport-google-oauth';
+import GitHubStrategy from 'passport-github2';
 
 //import * as authController from '../controllers/user_auth';
 
@@ -12,6 +13,10 @@ export default router;
 router.get('/', userController.index);
 router.get('/login', userController.login);
 
+
+/**
+ * GOOGLE OAUTH CONFIG
+ */
 // Passport setup
 passport.use(new GoogleStrategy.OAuth2Strategy({
   clientID: '741961836801-v58t7bv2t08jglenlj9vlvh6usr57l2d.apps.googleusercontent.com',
@@ -45,5 +50,30 @@ router.get('/auth/google/callback',
   }
 );
 
-//router.get('/auth/github', authController.authGoogle);
-//router.get('/auth/github/callback', authController.authGoogleCallback);
+/**
+ * GITHUB OAUTH CONFIG
+ */
+passport.use(new GitHubStrategy({
+  clientID: '1e3f16cdf86532f5e0a5',
+  clientSecret: 'b925c897f3968fa2dcea10ba88d530beaa6239de',
+  callbackURL: 'https://pmpc1465.npm.ac.uk/geona/user/auth/github/callback',
+},
+function(token, tokenSecret, profile, done) {
+  // User.findOrCreate({ githubId: profile.id }, function (err, user) {
+  //   return done(err, user);
+  // });
+  return done(null, profile);
+}
+));
+
+router.get('/auth/github',
+  passport.authenticate('github', { scope: ['https://www.githubapis.com/auth/plus.login'] })
+);
+
+router.get('/auth/github/callback',
+  passport.authenticate('github', { failureRedirect: '/login', successRedirect: '/success' }),
+  function(req, res) {
+    res.redirect('/');
+  }
+);
+
