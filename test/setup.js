aa76021f991d9f9639850f94c9410ed592d305c4
Ben Calton <bac@pml.ac.uk>
2017-09-28 18:45:58 +0100

    WIP: Add mocks and events to the test setup

--- test/setup.js
+++ test/setup.js
@@ -1,4 +1,8 @@
 import path from 'path';
+import httpMocks from 'node-mocks-http';
+import eventEmitter from 'eventemitter3';
+
+import * as config from '../src/server/config';
 
 before(function() {
   // Setup global test variables for use in tests
@@ -7,4 +11,28 @@ before(function() {
     resPath: path.join(__dirname, '../test_dependencies/resources'),
     expPath: path.join(__dirname, '../test_dependencies/expected'),
   };
+
+  // add functios for creating mock request and response objects
+  global.mocks = {
+    createReq: function() {
+      return httpMocks.createRequest({
+        headers: {
+          host: '127.0.0.1:' + config.server.get('port'),
+        },
+        ip: '127.0.0.1',
+        session: {
+          passport: undefined,
+        },
+      });
+    },
+    createRes: function() {
+      return httpMocks.createResponse({
+        eventEmitter: eventEmitter,
+      });
+    },
+  };
+});
+
+after(function() {
+  // tasks to clean up after the testing
 });
