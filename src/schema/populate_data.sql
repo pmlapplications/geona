a7a087dc41c7f9e2256337643947bb9f49b5e495
Ben Calton <bac@pml.ac.uk>
2017-10-05 18:12:23 +0100

    Separate schema from data

---
+++ src/schema/populate_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO dbConfig (version) VALUES (1);
+INSERT INTO dbConfig (version) VALUES (2);
+INSERT INTO dbConfig (version) VALUES (3);
+INSERT INTO dbConfig (version) VALUES (4);
+INSERT INTO dbConfig (version) VALUES (5);
