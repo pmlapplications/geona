f8b81b28a0d09d1bdf0ffeeaf4ae84ccba1252e3
Ben Calton <bac@pml.ac.uk>
2017-10-06 13:42:12 +0100

    No longer any need for the data file to be separate

--- src/schema/populate_data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO dbConfig (version) VALUES (1);
-INSERT INTO dbConfig (version) VALUES (2);
-INSERT INTO dbConfig (version) VALUES (3);
-INSERT INTO dbConfig (version) VALUES (4);
-INSERT INTO dbConfig (version) VALUES (5);
