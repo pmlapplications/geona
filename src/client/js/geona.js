faa6d536f1ca4147bc380b4b1216068ec65df043
Nicholas J R Bailey <njrbailey@outlook.com>
2018-05-09 15:37:45 +0100

    Fixed maxExtent Infinity getting saved + stringified as null

--- src/client/js/geona.js
+++ src/client/js/geona.js
@@ -100,7 +100,65 @@ export class Geona {
     geonaState.controls.timeline = this.gui.timePanel.config;
     geonaState.controls.menu = this.gui.mainMenu.config;
 
+    // Infinity will get converted to null when stringified so we need to check all the viewSettings and sanitize them
+    for (let basemapServer of geonaState.map.basemapLayers) {
+      if (basemapServer.layers) {
+        for (let basemap of basemapServer.layers) {
+          if (basemap.viewSettings && basemap.viewSettings.maxExtent) {
+            if (basemap.viewSettings.maxExtent.minLon === -Infinity) {
+              basemap.viewSettings.maxExtent.minLon = 'sanitized.-Infinity';
+            }
+          }
+          if (basemap.viewSettings && basemap.viewSettings.maxExtent) {
+            if (basemap.viewSettings.maxExtent.maxLon === Infinity) {
+              basemap.viewSettings.maxExtent.maxLon = 'sanitized.Infinity';
+            }
+          }
+        }
+      }
+    }
+    for (let bordersServer of geonaState.map.bordersLayers) {
+      if (bordersServer.layers) {
+        for (let borders of bordersServer.layers) {
+          if (borders.viewSettings && borders.viewSettings.maxExtent) {
+            if (borders.viewSettings.maxExtent.minLon === -Infinity) {
+              borders.viewSettings.maxExtent.minLon = 'sanitized.-Infinity';
+            }
+          }
+          if (borders.viewSettings && borders.viewSettings.maxExtent) {
+            if (borders.viewSettings.maxExtent.maxLon === Infinity) {
+              borders.viewSettings.maxExtent.maxLon = 'sanitized.Infinity';
+            }
+          }
+        }
+      }
+    }
+    for (let dataServer of geonaState.map.dataLayers) {
+      if (dataServer.layers) {
+        for (let data of dataServer.layers) {
+          if (data.viewSettings && data.viewSettings.maxExtent) {
+            if (data.viewSettings.maxExtent.minLon === -Infinity) {
+              data.viewSettings.maxExtent.minLon = 'sanitized.-Infinity';
+            }
+          }
+          if (data.viewSettings && data.viewSettings.maxExtent) {
+            if (data.viewSettings.maxExtent.maxLon === Infinity) {
+              data.viewSettings.maxExtent.maxLon = 'sanitized.Infinity';
+            }
+          }
+        }
+      }
+    }
+    if (geonaState.map.viewSettings.maxExtent.minLon === -Infinity) {
+      geonaState.map.viewSettings.maxExtent.minLon = 'sanitized.-Infinity';
+    }
+    if (geonaState.map.viewSettings.maxExtent.maxLon === Infinity) {
+      geonaState.map.viewSettings.maxExtent.maxLon = 'sanitized.Infinity';
+    }
+
+
     console.log(geonaState); // removeme
+
 
     // Save the state into the browser cache (or database?)
     this.geonaSaveState = JSON.stringify(geonaState); // TODO put this in the cache
@@ -113,6 +171,62 @@ export class Geona {
   loadGeonaState(geonaStateJson) {
     let geonaState = JSON.parse(geonaStateJson);
 
+    // Infinity will get converted to null when stringified so we need to check all the viewSettings and sanitize them
+    for (let basemapServer of geonaState.map.basemapLayers) {
+      if (basemapServer.layers) {
+        for (let basemap of basemapServer.layers) {
+          if (basemap.viewSettings && basemap.viewSettings.maxExtent) {
+            if (basemap.viewSettings.maxExtent.minLon === 'sanitized.-Infinity') {
+              basemap.viewSettings.maxExtent.minLon = -Infinity;
+            }
+          }
+          if (basemap.viewSettings && basemap.viewSettings.maxExtent) {
+            if (basemap.viewSettings.maxExtent.maxLon === 'sanitized.Infinity') {
+              basemap.viewSettings.maxExtent.maxLon = Infinity;
+            }
+          }
+        }
+      }
+    }
+    for (let bordersServer of geonaState.map.bordersLayers) {
+      if (bordersServer.layers) {
+        for (let borders of bordersServer.layers) {
+          if (borders.viewSettings && borders.viewSettings.maxExtent) {
+            if (borders.viewSettings.maxExtent.minLon === 'sanitized.-Infinity') {
+              borders.viewSettings.maxExtent.minLon = -Infinity;
+            }
+          }
+          if (borders.viewSettings && borders.viewSettings.maxExtent) {
+            if (borders.viewSettings.maxExtent.maxLon === 'sanitized.Infinity') {
+              borders.viewSettings.maxExtent.maxLon = Infinity;
+            }
+          }
+        }
+      }
+    }
+    for (let dataServer of geonaState.map.dataLayers) {
+      if (dataServer.layers) {
+        for (let data of dataServer.layers) {
+          if (data.viewSettings && data.viewSettings.maxExtent) {
+            if (data.viewSettings.maxExtent.minLon === 'sanitized.-Infinity') {
+              data.viewSettings.maxExtent.minLon = -Infinity;
+            }
+          }
+          if (data.viewSettings && data.viewSettings.maxExtent) {
+            if (data.viewSettings.maxExtent.maxLon === 'sanitized.Infinity') {
+              data.viewSettings.maxExtent.maxLon = Infinity;
+            }
+          }
+        }
+      }
+    }
+    if (geonaState.map.viewSettings.maxExtent.minLon === 'sanitized.-Infinity') {
+      geonaState.map.viewSettings.maxExtent.minLon = -Infinity;
+    }
+    if (geonaState.map.viewSettings.maxExtent.maxLon === 'sanitized.Infinity') {
+      geonaState.map.viewSettings.maxExtent.maxLon = Infinity;
+    }
+
     this.map = undefined;
     this.gui = undefined;
     this.eventManager = undefined;
@@ -124,6 +238,8 @@ export class Geona {
     this.config.set('map', geonaState.map);
     this.config.set('intro', geonaState.intro);
     this.config.set('controls', geonaState.controls);
+
+    console.log(this.config.get('map'));
 
     this.geonaDiv.empty();
     this.geonaDiv.removeClass();
