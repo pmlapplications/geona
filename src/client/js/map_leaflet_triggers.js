a82f37af3e2f64bf1aa9a64c8092505bb21adf3e
Nicholas J R Bailey <njrbailey@outlook.com>
2018-05-23 10:22:15 +0100

    Removed unnecessary lines

--- src/client/js/map_leaflet_triggers.js
+++ src/client/js/map_leaflet_triggers.js
@@ -1,10 +1,8 @@
 export function registerTriggers(eventManager, geonaDiv, leafletMap) {
   leafletMap.on('zoomend', () => {
-    console.log('map changed');
     eventManager.trigger('mapLeaflet.updateConfig');
   });
   leafletMap.on('moveend', () => {
-    console.log('map changed');
     eventManager.trigger('mapLeaflet.updateConfig');
   });
 }
--- src/client/js/map_openlayers_triggers.js
+++ src/client/js/map_openlayers_triggers.js
@@ -1,6 +1,5 @@
 export function registerTriggers(eventManager, geonaDiv, openLayersMap) {
   openLayersMap.getView().on('change', () => {
-    console.log('map changed');
     eventManager.trigger('mapOpenlayers.updateConfig');
   });
 }
